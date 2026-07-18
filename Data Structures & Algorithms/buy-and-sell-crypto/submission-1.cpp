98dcbf7aee18f6a3161c3696e65c28a3841fe45a
Aashish Singh <aashishnsingh20060@gmail.com>
2026-07-19 00:10:04 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        int mini = prices[0];
+        int maxProf = 0;
+        for(int i=1;i<n;i++){
+            mini = min(mini,prices[i]);
+            maxProf = max(maxProf,(prices[i]-mini));
+        }
+        return maxProf;
+    }
+};
