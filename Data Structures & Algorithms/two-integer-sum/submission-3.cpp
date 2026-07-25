8502bc9f10d8f52856013ef69eb8144c1b7332c5
Aashish Singh <aashishnsingh20060@gmail.com>
2026-07-25 15:19:26 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+
+        // Store value -> index
+        for(int i = 0;i<n;i++){
+            int diff = target - nums[i];  // finds diff between target and curr element
+
+            if(mp.find(diff) != mp.end()){  // if diff is present in map then return index of current element and index of diff
+                return {mp[diff],i};
+            }
+            mp[nums[i]] = i;  // For every number it's index will be it's key
+        }
+        return {};  // if nothing is found then return empty
+    }
+};
